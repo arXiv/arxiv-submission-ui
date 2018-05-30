823cb753e88fafa9b73a3147ebc4eed8be303610
Jaimie Murdock <jaimie.murdock@gmail.com>
2018-05-30 11:51:03 -0400

    adding stubs for controlelrs

---
+++ submit/controllers/verify_user.py
@@ -0,0 +1,11 @@
+""" 
+Controller for verify_user action. 
+
+Creates an event of type `core.events.event.VerifyContactInformation`
+"""
+
+from core.events.event import VerifyContactInformation
+
+def verify_user():
+    pass
+

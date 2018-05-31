4de1cdb6213e2f950988d44fe606484932f25d78
Jaimie Murdock <jaimie.murdock@gmail.com>
2018-05-30 23:19:12 -0400

    removing sketch of verify_user code

--- submit/controllers/verify_user.py
+++ submit/controllers/verify_user.py
@@ -13,20 +13,6 @@ from arxiv import status
 
 # from arxiv-submission-core.events.event import VerifyContactInformation
 
-"""
- def verify_user(data):
-    if method == 'GET':
-        # generate new wtform
-        pass
-    elif method == 'POST':
-        # instantiate new form with POST data
-        # is form valid?
-        if form.validate():
-            # create event
-            # generate redirect to next step
-            pass
-"""
-
 Response = Tuple[Dict[str, Any], int, Dict[str, Any]]
 
 

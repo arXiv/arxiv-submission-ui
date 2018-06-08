0e9ea5d7410b15ce9f408b46263b3013fd16998a
Jaimie Murdock <jaimie.murdock@gmail.com>
2018-06-08 15:55:26 -0400

    adding imports

--- submit/controllers/__init__.py
+++ submit/controllers/__init__.py
@@ -1,4 +1,6 @@
 from .verify_user import verify_user
 from .authorship import authorship
+from .license import license
+from .policy import policy
 
-__all__ = ['verify_user', 'authorship']
+__all__ = ['verify_user', 'authorship', 'license', 'policy']

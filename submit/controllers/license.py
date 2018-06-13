21abb512a1f07c7e8185e5dbfd339c6cec675589
Jaimie Murdock <jaimie.murdock@gmail.com>
2018-06-13 15:24:37 -0400

    adding validation

--- submit/controllers/license.py
+++ submit/controllers/license.py
@@ -9,6 +9,7 @@ from typing import Tuple, Dict, Any
 from flask import url_for
 from wtforms import Form
 from wtforms.fields import RadioField
+from wtforms.validators import InputRequired
 
 from arxiv import status
 from arxiv.base import logging
@@ -53,17 +54,21 @@ def license(request_params: dict, submission_id: int) -> Response:
 
 class LicenseForm(Form):
     """Generate form to select license."""
-    license = RadioField(u'Select a license', choices=[
-        ('http://arxiv.org/licenses/nonexclusive-distrib/1.0/', 
-            'arXiv.org perpetual, non-exclusive license to distribute this'
-            'article'),
-        ('http://creativecommons.org/licenses/by-nc-sa/4.0/',
-            'Creative Commons Attribution-Noncommercial-ShareAlike license '
-            '(CC BY-NC-SA 4.0)'),
-        ('http://creativecommons.org/licenses/by-sa/4.0/',
-            'Creative Commons Attribution-ShareAlike license (CC BY-SA 4.0)'),
-        ('http://creativecommons.org/licenses/by/4.0/', 
-            'Creative Commons Attribution license (CC BY 4.0)'),
-        ('http://creativecommons.org/publicdomain/zero/1.0/',
-            'Creative Commons Public Domain Declaration (CC0 1.0)'),
-        ('',  'None of the above licenses apply')])
+    license = RadioField(
+        u'Select a license',
+        choices=[
+            ('http://arxiv.org/licenses/nonexclusive-distrib/1.0/', 
+                'arXiv.org perpetual, non-exclusive license to distribute this'
+                'article'),
+            ('http://creativecommons.org/licenses/by-nc-sa/4.0/',
+                'Creative Commons Attribution-Noncommercial-ShareAlike license '
+                '(CC BY-NC-SA 4.0)'),
+            ('http://creativecommons.org/licenses/by-sa/4.0/',
+                'Creative Commons Attribution-ShareAlike license (CC BY-SA 4.0)'),
+            ('http://creativecommons.org/licenses/by/4.0/', 
+                'Creative Commons Attribution license (CC BY 4.0)'),
+            ('http://creativecommons.org/publicdomain/zero/1.0/',
+                'Creative Commons Public Domain Declaration (CC0 1.0)'),
+            ('',  'None of the above licenses apply')
+        ],
+        validators=[InputRequired('Please select a license')])

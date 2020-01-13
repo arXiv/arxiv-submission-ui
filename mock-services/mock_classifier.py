a9bad8da012a593d3297c81185e4e2bcf1efe330
David Fielding <dlf2@cornell.edu>
2020-01-13 16:02:43 -0500

    More lint cleanup

--- mock-services/mock_classifier.py
+++ mock-services/mock_classifier.py
@@ -1,3 +1,4 @@
+"""Mock endpoint for classifier service."""
 from flask import Flask, jsonify
 
 application = Flask(__name__)
@@ -5,6 +6,7 @@ application = Flask(__name__)
 
 @application.route('/status', methods=['GET'])
 def get_status():
+    """Return classifier status."""
     return 'ok'
 
 
@@ -12,46 +14,46 @@ def get_status():
 def classify():
     """Generate classification results."""
     return jsonify({
-      "classifier": [
-        {
-          "category": "physics.comp-ph",
-          "logodds": -0.11,
-          "topwords": [
-            {"processors": 13},
-            {"fft": 13},
-            {"decyk": 4},
-            {"fast fourier transform": 7},
-            {"parallel": 10}
-          ]
+        "classifier": [
+            {
+                "category": "physics.comp-ph",
+                "logodds": -0.11,
+                "topwords": [
+                    {"processors": 13},
+                    {"fft": 13},
+                    {"decyk": 4},
+                    {"fast fourier transform": 7},
+                    {"parallel": 10}
+                ]
+            },
+            {
+                "category": "cs.MS",
+                "logodds": -0.14,
+                "topwords": [
+                    {"fft": 13},
+                    {"processors": 13},
+                    {"fast fourier transform": 7},
+                    {"parallel": 10},
+                    {"processor": 7}
+                ]
+            },
+            {
+                "category": "math.NA",
+                "logodds": -0.16,
+                "topwords": [
+                    {"fft": 13},
+                    {"fast fourier transform": 7},
+                    {"algorithm": 6},
+                    {"ux": 4},
+                    {"multiplications": 5}
+                ]
+            }
+        ],
+        "counts": {
+            "chars": 15107,
+            "pages": 12,
+            "stops": 804,
+            "words": 2860
         },
-        {
-          "category": "cs.MS",
-          "logodds": -0.14,
-          "topwords": [
-            {"fft": 13},
-            {"processors": 13},
-            {"fast fourier transform": 7},
-            {"parallel": 10},
-            {"processor": 7}
-          ]
-        },
-        {
-          "category": "math.NA",
-          "logodds": -0.16,
-          "topwords": [
-            {"fft": 13},
-            {"fast fourier transform": 7},
-            {"algorithm": 6},
-            {"ux": 4},
-            {"multiplications": 5}
-          ]
-        }
-      ],
-      "counts": {
-        "chars": 15107,
-        "pages": 12,
-        "stops": 804,
-        "words": 2860
-      },
-      "flags": {}
+        "flags": {}
     })

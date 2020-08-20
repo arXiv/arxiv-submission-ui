cac9e1fb0a8aed65adaaa0420e24de6b53245cd0
Brian D. Caruso <bdc34@cornell.edu>
2020-08-20 17:34:47 -0400

    Working on running integration test in docker
    This has a docker compose with the integration test.
    The test runs, waits for the submission-ui and gets to the file upload
    stage.
    
    ARXIVNG-3455

--- integrationtest.sh
+++ integrationtest.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-#export SERVICES='filemanager compiler-api compiler-worker submission-worker submission-agent submission-ui submission-preview legacy-filesystem plaintext-worker plaintext-api mailhog submission-ui-redis  submission-ui-maria   submission-agent-maria   filemanager-maria   mock-classifier   submission-localstack integration-test'
+
+# Shell script to run the docker compose with the integration test.
+
 export AWS_ACCESS_KEY_ID=fookey
 export AWS_SECRET_ACCESS_KEY=foosecretkey
 #export CONVERTER_DOCKER_IMAGE=626657773168.dkr.ecr.us-east-1.amazonaws.com/arxiv/converter:0.9
@@ -7,5 +9,5 @@ export CONVERTER_DOCKER_IMAGE=626657773168.dkr.ecr.us-east-1.amazonaws.com/arxiv
 export DIND_SOURCE_ROOT=/tmp/subui-quickstart-compiler-dind-src-root
 mkdir -p $DIND_SOURCE_ROOT
 #docker-compose rm -vf
-#docker-compose build
+docker-compose -f docker-compose.yml -f docker-compose.intgtest.yml build
 docker-compose -f docker-compose.yml -f docker-compose.intgtest.yml up

4dbb79c3bc7768795cf721c9539bd0f9dcd1e0b0
Brian D. Caruso <bdc34@cornell.edu>
2020-08-21 14:01:22 -0400

    Changing URL for integration test dockerfile

--- integrationtest.sh
+++ integrationtest.sh
@@ -8,6 +8,9 @@ export AWS_SECRET_ACCESS_KEY=foosecretkey
 export CONVERTER_DOCKER_IMAGE=626657773168.dkr.ecr.us-east-1.amazonaws.com/arxiv/converter-2016:0.1.5
 export DIND_SOURCE_ROOT=/tmp/subui-quickstart-compiler-dind-src-root
 mkdir -p $DIND_SOURCE_ROOT
-#docker-compose rm -vf
+docker-compose rm -vf
 docker-compose -f docker-compose.yml -f docker-compose.intgtest.yml build
 docker-compose -f docker-compose.yml -f docker-compose.intgtest.yml up
+
+# This doesn't work yet because a lot of the services exit, like arxiv-submission-worker
+#docker-compose -f docker-compose.yml -f docker-compose.intgtest.yml up --exit-code-from integration-test
